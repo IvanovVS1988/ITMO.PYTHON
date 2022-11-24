0bd9fbc4f13208722d84e81113ed342968076ad3
Vladimir Ivanov <ivanovladimir1988@gmail.com>
2022-11-24 18:18:21 +0400

    Input constants and format output time

--- Lab01/task1.py
+++ Lab01/task1.py
@@ -1,26 +1,26 @@
 # -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
 
 import math
 
-d1 = 3*float(input("Input distance d1 in yards: "))
-d2 = float(input("Input distance d2 in feet: "))
-h = 3*float(input("Input h in yards: "))
-v_sand = 5280*float(input("Input speed v_sand in m/h: "))/3600
-n = float(input("Input n cofficient: "))
-theta1 = float(input("Input angel in deegres: "))/57.295782
+YARDS_PER_FOOT = 3
+FETT_PER_MILE = 5280
+SECONDS_PER_HOUR = 3600
+DEGREES_PER_RADIAN = 57.295782
 
-L1 = d1/math.cos(theta1)
-x = L1*math.sin(theta1)
+d1 = YARDS_PER_FOOT * float(input("Input distance d1 in yards: "))
+d2 = float(input("Input distance d2 in feet: "))
+h = YARDS_PER_FOOT * float(input("Input h in yards: "))
+v_sand = FETT_PER_MILE * float(input("Input speed v_sand in m/h: ")) / SECONDS_PER_HOUR
+n = float(input("Input n cofficient: "))
+theta1 = float(input("Input angel in deegres: ")) / DEGREES_PER_RADIAN
+
+L1 = d1 / math.cos(theta1)
+x = L1 * math.sin(theta1)
 
 if x > h:
     print("Input angel is to big")
 else:
-    L2 = math.sqrt((h-x)**2+d2**2)
-    time = L1/v_sand + L2/(v_sand/n)
-    print("Спасатель достигнет утопающего через: " + str(time) + " секунды")
+    L2 = math.sqrt((h-x)**2 + d2**2)
+    time = L1 / v_sand + L2 / (v_sand/n)
+    print(f"Rescuer will save drowning human after: %.1f seconds" %time)
     
